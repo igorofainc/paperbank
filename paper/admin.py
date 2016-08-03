fb2036160c25aa3c3ea2b308c3f8a4a9cc357972
upatricck <patrickuwonkunda@gmail.com>
2016-08-03 10:26:30 +0200

    adding the subject model to the admin area

--- paper/admin.py
+++ paper/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Paper
+from .models import Paper, Subject
 
 
 # Register your models here.
 admin.site.register(Paper)
+admin.site.register(Subject)
