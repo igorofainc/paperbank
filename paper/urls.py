1dc0879dbccc9b468e29dc4488667b87d61606a3
upatricck <patrickuwonkunda@gmail.com>
2016-10-23 19:20:50 +0200

    fixed problem with regex values containing spaces, in tags filtering url

--- paper/urls.py
+++ paper/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.landing, name='landing_page'),
     url(r'^papers$', views.main, name='main_page'),
-    url(r'^tag/(\w{0,50})$', views.filter_by_tag, name='filter_by_tag'),
+    url(r'^tag/([\w ]+)$', views.filter_by_tag, name='filter_by_tag'),
     url(r'^search', views.search, name='search'),
 ]
 
