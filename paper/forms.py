13ffa65377ac6851d8d1b793e6dc778153456478
upatricck <patrickuwonkunda@gmail.com>
2017-02-17 15:37:31 +0200

    Add tags field in to the paper upload form

--- paper/forms.py
+++ paper/forms.py
@@ -1,11 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Paper
 
-class PaperForm(ModelForm):
+class PaperForm(forms.ModelForm):
     """
     Form for uploading papers
     """
+    tags = forms.CharField(help_text="separate tags with a comma, ex: maths, ldk")
+
     class Meta:
        model = Paper
        exclude = ('created_date', 'tags')
