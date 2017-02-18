48be22f90526efb6d085debf55cbbcdd982e48fc
Patrick Uwonkunda <patrickuwonkunda@gmail.com>
2017-02-18 18:29:47 +0200

    Created a serializer for paper models

---
+++ paper/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from paper.models import Paper
+
+class PaperSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Paper model class
+    """
+    class Meta:
+        model = Paper
+        fields = ('id', 'name')
